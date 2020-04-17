8e6eb8e85a63f0ef8d19f06f9dacd56088bc74c1
kccistc <kccistc@DESKTOP-E03M5PJ>
2020-04-17 17:49:01 +0900

    ok

---
+++ ActualPractice/src/test/TokenizerPractice.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.StringTokenizer;
+
+public class TokenizerPractice {
+
+	public static void main(String[] args) {
+		
+		 String str = "1:2:3:4:5";
+		 StringTokenizer s = new StringTokenizer(str,":");
+		 while(s.hasMoreTokens()) {
+			 System.out.println(s.nextToken());
+		 }
+
+	}
+
+}
